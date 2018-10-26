674e48212d820aa81c5435b1107a6fec449c4225
Ubuntu <ubuntu@ip-172-31-40-57.eu-central-1.compute.internal>
2018-10-26 13:06:04 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AWS Fargate with Terraform
+
+This repository contains configuration to deploy a Dockerized web server to AWS Fargate using Terraform.
+
---
+++ terraform/iam.tf
@@ -0,0 +1,93 @@
+resource "aws_iam_role" "ecs_autoscale_role" {
+  name               = "ecs_autoscale_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "application-autoscaling.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_autoscale_role_policy" {
+  name   = "ecs_autoscale_role_policy"
+  role   = "${aws_iam_role.ecs_autoscale_role.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ecs:DescribeServices",
+        "ecs:UpdateService"
+      ],
+      "Resource": [
+        "*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "cloudwatch:DescribeAlarms"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "ecs_task_assume" {
+  name = "ecs_task_assume"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "ecs_task_assume" {
+  name = "ecs_task_assume"
+  role = "${aws_iam_role.ecs_task_assume.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
